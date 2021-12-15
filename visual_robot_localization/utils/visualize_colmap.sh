48023708c8cccfeb17a7f6c98f8d5cc726a7d664
lauri <lauri.a.suomela@tuni.fi>
2021-12-15 17:01:51 +0200

    Updated utility script paths

--- visual_robot_localization/utils/visualize_colmap.sh
+++ visual_robot_localization/utils/visualize_colmap.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-image_folder=2021-11-15_13:37:47 #2021-10-06_16:43:21
-model_folder=dir+superpoint_aachen+superglue #netvlad+superpoint_aachen+superglue #sfm_superpoint+superglue
+SCRIPT=$(readlink -f "$0")
+CWD=$(dirname "$SCRIPT")
+
+# Point this to the directory which contains your gallery images
+image_folder=$CWD/../test/example_dir/
+
+# Check the name of the directory that contains the output of do_SfM.sh
+localization_method_name=netvlad+superpoint_aachen+superglue
+
+model_path=$image_folder/outputs/$localization_method_name/
+db_path=$model_path/database.db
+sfm_path=$model_path/sfm_$localization_method_name/
 
 colmap gui \
-    --database_path /image-gallery/$image_folder/outputs/$model_folder/sfm_$model_folder/database.db \
-    --image_path /image-gallery/$image_folder/ \
-    --import_path /image-gallery/$image_folder/outputs/$model_folder/sfm_$model_folder/ # only available in colmap 3.6
+    --database_path $db_path \
+    --image_path $image_folder \
+    --import_path $sfm_path
