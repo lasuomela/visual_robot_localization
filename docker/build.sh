e770e07d9660d0e32e12cb78c712b63caf9b1980
lauri <lauri.a.suomela@tuni.fi>
2021-12-16 15:28:46 +0200

    Updated docker build

--- docker/build.sh
+++ docker/build.sh
@@ -5,9 +5,20 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 ROS_DISTRO="foxy"
 BASE_IMAGE="ros:$ROS_DISTRO-ros-base"
+IMAGE_NAME="visual_robot_localization:0.0.1"
+
+while getopts r:b:t: flag
+do
+    case "${flag}" in
+        r) ROS_DISTRO=${OPTARG};;
+        b) BASE_IMAGE=${OPTARG};;
+        t) IMAGE_NAME=${OPTARG};;
+        *) error "Unexpected option ${flag}" ;;
+    esac
+done
 
 docker build \
-    -t visual_robot_localization:0.0.1 \
+    -t $IMAGE_NAME \
     --build-arg ROS_DISTRO=$ROS_DISTRO \
     --build-arg BASE_IMAGE=$BASE_IMAGE \
     -f Dockerfile ${SCRIPT_DIR}/..
